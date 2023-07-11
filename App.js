d4f607f487d03e5ba858ef61b396806e29bee494
Jugal Kishore <jugal.kishore@dotsquares.com>
2023-07-11 16:43:23 +0530

    changes 11-07-2023

--- App.js
+++ App.js
@@ -1,2 +1,10 @@
-import react from "react";
-import reactDom from "react-dom";
+import React from "react";
+import ReactDom from "react-dom/client";
+
+const main = React.createElement(
+  "h3",
+  { id: "headingMain" },
+  "This is Heading tag"
+);
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(main);
